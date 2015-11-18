2bb95477a5ad609a2309199cf583e8385368b9ff
Angel Popov on ThinkPad <angelpopov@labssoft.com.tp>
2015-11-18 19:21:13 +0200

    Added subspcriptions

--- stripe.js
+++ stripe.js
@@ -1,24 +1,56 @@
-var app=require('express')();
-var config=require('./config.js')
+var app = require('express')();
+var config = require('./config.js')
+var stripe = require('stripe')(config.private_key)
 var util=require('util');
 app.use(require('body-parser').urlencoded({extended:false}))
 app.get("/charge",function(req,res){
     res.send('<form action="/charge" method="POST">'+
-'  <script'+
-'    src="https://checkout.stripe.com/checkout.js" class="stripe-button"'+
-'    data-key="' + config.public_key + '"'+
-'    data-image="/img/documentation/checkout/marketplace.png"'+
-'    data-name="LeadSecure Site"'+
-'    data-description="video things"'+
-'    data-panel-label="pay once"'+
-'    data-email="some@email.com"' +
-'    data-amount="500"'+
-'    data-parameter="500"'+
-//'    data-bitcoin=true"'+
-'    data-locale="auto">'+
-'  </script>'+
-'</form>')
+	     '  <script'+
+	     '    src="https://checkout.stripe.com/checkout.js" class="stripe-button"'+
+	     '    data-key="' + config.public_key + '"'+
+	     '    data-image="/img/documentation/checkout/marketplace.png"'+
+	     '    data-name="LeadSecure Site"'+
+	     '    data-description="video things"'+
+	     '    data-panel-label="pay once"'+
+	     '    data-email="some@email.com"' +
+	     '    data-amount="500"'+
+	     '    data-parameter="500"'+
+	     //'    data-bitcoin=true"'+
+	     '    data-locale="auto">'+
+	     '  </script>'+
+	     '</form>'+
+	     '<form action="/subscribe" method="POST">'+
+	     '  <script'+
+	     '    src="https://checkout.stripe.com/checkout.js" class="stripe-button"'+
+	     '    data-key="' + config.public_key + '"'+
+	     '    data-image="/img/documentation/checkout/marketplace.png"'+
+	     '    data-name="LeadSecure Site"'+
+	     '    data-description="video things"'+
+	     '    data-panel-label="subscribe"'+
+	     '    data-email="some@email.com"' +
+	     '    data-amount="500"'+
+	     '    data-parameter="500"'+
+	     //'    data-bitcoin=true"'+
+	     '    data-locale="auto">'+
+	     '  </script>'+
+	     '</form>')
 })
+app.post("/subscribe", function(req,res){
+    var token=req.body.stripeToken;
+    var email=req.body.stripeEmail;
+    stripe.customers.create({
+	source: token,
+	plan: "1",
+	email: email
+    },function(err,data){
+	if(err){
+	    res.send("<html><body>Error detected:<p>"+err+'</p></html>');
+	} else {
+	    res.send("<html><body>You have been charged for 50 cents:<pre>"+util.inspect(data)+'</pre></html>');
+	}
+    })
+});
+
 app.post("/charge", function(req,res){
     var token=req.body.stripeToken;
     var email=req.body.stripeEmail;
@@ -37,4 +69,3 @@ app.post("/charge", function(req,res){
 });
 
 var sever=app.listen(3000,function(){console.log("Started... open browser at localhost:3000/charge")})
-
